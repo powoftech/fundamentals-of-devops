36b3d90433100b1399bb037049ab3dca9312749f
Phuong Dang <powoftech@gmail.com>
2025-06-13 09:23:29 +0700

    feat: finish chapter 2

---
+++ ch2/tofu/live/sample-app-github/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "sample_app_1" {
+  source  = "brikis98/devops/book//modules/ec2-instance"
+  version = "1.0.0"
+
+  name = "sample-app-tofu-1"
+}
+
+module "sample_app_2" {
+  source  = "brikis98/devops/book//modules/ec2-instance"
+  version = "1.0.0"
+
+  name = "sample-app-tofu-2"
+}
---
+++ ch2/tofu/live/sample-app/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "sample_app_1" {
+  source = "../../modules/ec2-instance"
+
+  name = "sample-app-tofu-1"
+}
+
+module "sample_app_2" {
+  source = "../../modules/ec2-instance"
+
+  name = "sample-app-tofu-2"
+}

788e7e4e7753cc887a9b645e351daaf229709462
Phuong Dang <powoftech@gmail.com>
2025-06-16 11:49:03 +0700

    feat: add IAM role outputs for plan and apply stages in Terraform configuration

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -17,6 +17,11 @@ module "iam_roles" {
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn
 
   enable_iam_role_for_testing = true
+  enable_iam_role_for_plan  = true
+  enable_iam_role_for_apply = true
+
+  tofu_state_bucket         = "fundamentals-of-devops-tofu-state-593793056386-us-east-1"
+  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state-593793056386-us-east-1"
 
   # TODO: fill in your own repo name here!
   github_repo      = "powoftech/fundamentals-of-devops"

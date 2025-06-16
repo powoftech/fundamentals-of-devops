d0494a83a60d565ad58de297f32d5a38f9d9764a
Phuong Dang <powoftech@gmail.com>
2025-06-16 10:16:00 +0700

    feat: add infrastructure tests and Lambda function setup with necessary configurations

---
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "oidc_provider" {
+  source  = "brikis98/devops/book//modules/github-aws-oidc"
+  version = "1.0.0"
+
+  provider_url = "https://token.actions.githubusercontent.com"
+}
+
+module "iam_roles" {
+  source  = "brikis98/devops/book//modules/gh-actions-iam-roles"
+  version = "1.0.0"
+
+  name              = "lambda-sample"
+  oidc_provider_arn = module.oidc_provider.oidc_provider_arn
+
+  enable_iam_role_for_testing = true
+
+  # TODO: fill in your own repo name here!
+  github_repo      = "powoftech/fundamentals-of-devops"
+  lambda_base_name = "lambda-sample"
+}
